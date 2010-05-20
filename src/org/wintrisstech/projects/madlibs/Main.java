37761a35d6063b099e40078173094b0af6d2b4e1
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-05-20 16:05:26 -0700

    Clean up project template

--- src/org/wintrisstech/projects/madlibs/Main.java
+++ src/org/wintrisstech/projects/madlibs/Main.java
@@ -1,21 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.wintrisstech.projects.madlibs;
 
-/**
- *
- * @author avh4
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 
 }
