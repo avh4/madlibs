c0e7bfc00317a5add38d9490b79fa6e6525a2c77
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-05-20 16:08:49 -0700

    The most basic Madlibs program

--- src/org/wintrisstech/projects/madlibs/Main.java
+++ src/org/wintrisstech/projects/madlibs/Main.java
@@ -1,9 +1,14 @@
 package org.wintrisstech.projects.madlibs;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        String noun = JOptionPane.showInputDialog("an article of clothing (plural)");
+
+        JOptionPane.showMessageDialog(null, "What big " + noun + " you have!");
     }
 
 }
