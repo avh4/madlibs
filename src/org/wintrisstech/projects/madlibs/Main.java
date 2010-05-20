89439909c80234759e4eb76c777162d8e0f1ffc9
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-05-20 16:20:27 -0700

    Add some more variables and text

--- src/org/wintrisstech/projects/madlibs/Main.java
+++ src/org/wintrisstech/projects/madlibs/Main.java
@@ -7,8 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         String noun = JOptionPane.showInputDialog("an article of clothing (plural)");
+        String color = JOptionPane.showInputDialog("a color");
+        String person = JOptionPane.showInputDialog("a famous person");
+        String animal = JOptionPane.showInputDialog("a type of animal");
+        String place = JOptionPane.showInputDialog("a scary place");
 
-        JOptionPane.showMessageDialog(null, "What big " + noun + " you have!");
+        JOptionPane.showMessageDialog(null,
+                "Little " + color + " Riding Hood was walking through " +
+                place + "\non her way to " + person + "'s house.  Little did " +
+                "she know that a sneaky\n" + animal + " had eaten poor " + person
+                + " and was lying in wait for her.");
     }
 
 }
