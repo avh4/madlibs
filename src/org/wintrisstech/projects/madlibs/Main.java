b1cfd044551d6c0aae6ce26be2c8791cb27dbf09
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-05-20 16:34:16 -0700

    Add an image

--- src/org/wintrisstech/projects/madlibs/Main.java
+++ src/org/wintrisstech/projects/madlibs/Main.java
@@ -1,10 +1,14 @@
 package org.wintrisstech.projects.madlibs;
 
+import java.net.URL;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        URL imageUrl = Main.class.getResource("Riding Hood.jpg");
 
         String noun = JOptionPane.showInputDialog("an article of clothing (plural)");
         String color = JOptionPane.showInputDialog("a color");
@@ -16,7 +20,10 @@ public class Main {
                 "Little " + color + " Riding Hood was walking through " +
                 place + "\non her way to " + person + "'s house.  Little did " +
                 "she know that a sneaky\n" + animal + " had eaten poor " + person
-                + " and was lying in wait for her.");
+                + " and was lying in wait for her.",
+                "A Story",
+                JOptionPane.INFORMATION_MESSAGE,
+                new ImageIcon(imageUrl));
     }
 
 }
